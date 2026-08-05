c9c41c0bacd7248a69e668a6e200cb88cb839c38
okky putra <okky.putra4@gmail.com>
2026-08-05 15:34:39 +0700

    auto: rc/abi.js (2026-08-05T08:34:39.012Z)

--- src/abi.js
+++ src/abi.js
@@ -123,3 +123,26 @@ export const swapRouterAbi = [
     type: 'function',
   },
 ];
+
+export const spokePoolAbi = [
+  {
+    type: 'function',
+    name: 'depositV3',
+    stateMutability: 'payable',
+    inputs: [
+      { internalType: 'address', name: 'depositor', type: 'address' },
+      { internalType: 'address', name: 'recipient', type: 'address' },
+      { internalType: 'address', name: 'inputToken', type: 'address' },
+      { internalType: 'address', name: 'outputToken', type: 'address' },
+      { internalType: 'uint256', name: 'inputAmount', type: 'uint256' },
+      { internalType: 'uint256', name: 'outputAmount', type: 'uint256' },
+      { internalType: 'uint256', name: 'destinationChainId', type: 'uint256' },
+      { internalType: 'address', name: 'exclusiveRelayer', type: 'address' },
+      { internalType: 'uint32', name: 'quoteTimestamp', type: 'uint32' },
+      { internalType: 'uint32', name: 'fillDeadline', type: 'uint32' },
+      { internalType: 'uint32', name: 'exclusivityDeadline', type: 'uint32' },
+      { internalType: 'bytes', name: 'message', type: 'bytes' },
+    ],
+    outputs: [],
+  },
+];
